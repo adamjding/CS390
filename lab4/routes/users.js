2e7952ca7cdba7c126791f5ec0f12e45934fe9f1
Adam Ding <ding183@purdue.edu>
2019-11-04 19:52:43 -0500

    Attempting to work on GET methods

--- lab4/routes/users.js
+++ lab4/routes/users.js
@@ -1,30 +1,35 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const mongoose = require('mongoose');
 
 router.get('/', async (req, res) => {
-  // TODO: return all users
-  
+  // TODO: return all users;
+  var myUsers = [];
+  console.log(req); 
+  console.log("Idk");
+  res.send("henlooo");
 })
 
 router.post('/', async (req, res) => {
   // TODO: create a user
 })
 
-router.get('/:id', getUser, (req, res) => {
+router.get('/:id', (req, res) => {
   // TODO: return a single user by id
 })
 
-router.put('/:id', getUser, async (req, res) => {
+router.put('/:id', async (req, res) => {
   // TODO:update a user
 })
 
-router.delete('/:id', getUser, async (req, res) => {
+router.delete('/:id', async (req, res) => {
   // TODO:delete a user
 })
 
-async function getUser(req, res, next) {
-  // Not required, but could be used to get users with a specific id or all users
-}
+// async function getUser(req, res, next) {
+//   // Not required, but could be used to get users with a specific id or all users
+  
+// }
 
 module.exports = router 
