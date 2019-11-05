d0cd000f7c999cde0f421601661033d20a390921
Adam Ding <ding183@purdue.edu>
2019-11-04 20:53:31 -0500

    Lab 4 done

--- lab4/routes/users.js
+++ lab4/routes/users.js
@@ -5,26 +5,78 @@ const mongoose = require('mongoose');
 
 router.get('/', async (req, res) => {
   // TODO: return all users;
-  var myUsers = [];
-  console.log(req); 
-  console.log("Idk");
-  res.send("henlooo");
+  User.find({}, (err, users) => {
+    if(err) {
+      res.sendStatus(500);
+      return;
+    }
+    res.send(users);
+  });
 })
 
 router.post('/', async (req, res) => {
   // TODO: create a user
+  let newUser = new User({
+    name: req.body.name,
+    age: req.body.age,
+    occupation: req.body.occupation
+  });
+  newUser.save(async err => {
+    if(err) {
+      res.sendStatus(400);
+    } else {
+      res.sendStatus(201);
+    }
+  });
 })
 
 router.get('/:id', (req, res) => {
   // TODO: return a single user by id
+  User.findOne({_id: req.params.id}, (err, user) => {
+    if(err) {
+      res.sendStatus(404);
+      return;
+    }
+    res.send(user);
+    return;
+  });
 })
 
 router.put('/:id', async (req, res) => {
   // TODO:update a user
+  User.findById(req.params.id, (err, user) => {
+    if(err || user === null) {
+      res.sendStatus(404);
+      return;
+    }
+    if(req.body.name) {
+      user.name = req.body.name;
+    }
+    if(req.body.age) {
+      user.age = req.body.age;
+    }
+    if(req.body.occupation) {
+      user.occupation = req.body.occupation;
+    }
+    user.save((err) => {
+      if(err) {
+        res.sendStatus(500);
+        return;
+      }
+      res.sendStatus(200);
+    });
+  });
 })
 
 router.delete('/:id', async (req, res) => {
   // TODO:delete a user
+  User.findByIdAndDelete(req.params.id, (err, user) => {
+    if(err) {
+      res.sendStatus(404);
+    } else {
+      res.sendStatus(200);
+    }
+  });
 })
 
 // async function getUser(req, res, next) {
