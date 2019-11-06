9faa02385d284ac1112830c66f01ea9db7ce44f1
Adam Ding <ding183@purdue.edu>
2019-11-05 22:52:31 -0500

    Username and password fields now can get input and store them in a variable

--- gpa-calc/src/LoginPage.js
+++ gpa-calc/src/LoginPage.js
@@ -7,10 +7,27 @@ class LoginPage extends React.Component {
 
     printUserName = () => {
         console.log(this.state.username);
+        console.log(this.state.password);
+        if(this.state.username === "" && this.state.password === "") {
+            console.log("Username and Password are empty");
+        }
+        else if(this.state.username === "") {
+            console.log("Username is empty");
+        }
+        else if(this.state.password === "") {
+            console.log("Password is empty");
+        }
+        else {
+            console.log("Fields are filled out");
+        }
     }
 
-    getUserName = () => {
+    setUsername = () => {
         this.setState({username: document.getElementById("UsernameText").value});
+    }
+
+    setPassword = () => {
+        this.setState({password: document.getElementById("PasswordText").value});
     }
 
     render() {
@@ -19,15 +36,15 @@ class LoginPage extends React.Component {
                 <div className="LoginForm">
                     <label className="Label">Username</label>
                     <div className="UsernameInput">
-                        <input className="Input" type="text" maxLength="50" id="UsernameText"></input>
+                        <input className="Input" type="text" maxLength="50" id="UsernameText" onChange={this.setUsername}></input>
                     </div>
 
                     <label className="Label">Password</label>
                     <div className="PasswordInput">
-                        <input className="Input" type="text" maxLength="50" id="Passwordtext"></input>
+                        <input className="Input" type="password" maxLength="50" id="PasswordText" onChange={this.setPassword}></input>
                     </div>
 
-                    <button className="LoginButton">Login</button>
+                    <button className="LoginButton" onClick={this.printUserName}>Login</button>
                 </div>
             </div>
         );
